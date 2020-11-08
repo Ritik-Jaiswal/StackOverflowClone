2d1ff0b4fb7994c30c69028a6b475e35d52966c7
Ritik-Jaiswal <ritikjaiswal1709@gmail.com>
2020-11-08 18:14:03 +0530

    Added models classes for each table and added the DbContext class

---
+++ StackOverflowCloneProject.DomainModels/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowCloneProject.DomainModels
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //automatically generated while inserting
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}

056a6570b4b9bf27cf95bf6ee2b7e6cec234a2aa
Ritik-Jaiswal <ritikjaiswal1709@gmail.com>
2020-11-08 19:43:46 +0530

    ViewModel : added classes and added reference

---
+++ StackOverflowCloneProject.ViewModels/CategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowCloneProject.ViewModels
+{
+    public class CategoryViewModel
+    {
+        [Required]
+        public int CategoryID { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+    }
+}
---
+++ StackOverflowCloneProject.ViewModels/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowCloneProject.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [RegularExpression(@"(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z ]*$")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Mobile { get; set; }
+    }
+}

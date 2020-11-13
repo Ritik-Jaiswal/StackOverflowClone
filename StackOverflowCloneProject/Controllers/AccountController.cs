cae89e37a29534c93f9702b2de91d1fe1c53bfca
Ritik-Jaiswal <ritikjaiswal1709@gmail.com>
2020-11-13 21:10:19 +0530

    Register View Added

---
+++ StackOverflowCloneProject/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using StackOverflowCloneProject.ServiceLayer;
+using StackOverflowCloneProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StackOverflowCloneProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        IUsersService us;
+
+        public AccountController(IUsersService us)
+        {
+            this.us = us;
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel rvm)
+        {
+            if (ModelState.IsValid)
+            {
+                int uid = this.us.InsertUser(rvm);
+                Session["CurrentUserID"] = uid;
+                Session["CurrentUserName"] = rvm.Name;
+                Session["CurrentUserEmail"] = rvm.Email;
+                Session["CurrentUserPassword"] = rvm.Password;
+                Session["CurrentUserIsAdmin"] = false;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("x", "Invalid data");
+                return View();
+            }
+        }
+    }
+}

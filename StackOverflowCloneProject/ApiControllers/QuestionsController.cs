3e2b64b7f7338b566156527e51631cccc5130137
Ritik-Jaiswal <ritikjaiswal1709@gmail.com>
2020-11-20 20:15:24 +0530

    Up Vote / Down Vote added

---
+++ StackOverflowCloneProject/ApiControllers/QuestionsController.cs
@@ -0,0 +1,25 @@
+﻿using StackOverflowCloneProject.ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StackOverflowCloneProject.ApiControllers
+{
+    public class QuestionsController : ApiController
+    {
+        IAnswersService asr;
+
+        public QuestionsController(IAnswersService asr)
+        {
+            this.asr = asr;
+        }
+
+        public void Post(int AnswerID, int UserID, int value)
+        {
+            this.asr.UpdateAnswerVotesCount(AnswerID, UserID, value);
+        }
+    }
+}
